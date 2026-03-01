b73c9500b1faafbcf469121b93d7a3ae28eafee2
Purav Patel <patel.purav2024@vitstudent.ac.in>
2026-03-01 16:25:59 +0530

    Create event_handler.js

---
+++ event_handler.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    
+    // Button click event to trigger the toggle
+    $("#toggle-btn").click(function() {
+        
+        // Switching the visibility of the paragraph
+        $(".info-text").toggle();
+        
+    });
+
+});
