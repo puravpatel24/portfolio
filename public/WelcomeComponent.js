a5e55474735122814f0c29a7cc522da946b740a8
Purav Patel <patel.purav2024@vitstudent.ac.in>
2026-03-23 15:41:09 +0530

    Create WelcomeComponent.js

---
+++ public/WelcomeComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function WelcomeComponent() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello World... Welcome</h1>
+    </div>
+  );
+}
+
+export default WelcomeComponent;
